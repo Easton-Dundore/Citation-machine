d32a14ce8e592a3209f67556a4682f779d97491a
Easton-Dundore <78653755+Easton-Dundore@users.noreply.github.com>
2021-03-09 10:13:31 -0500

    Add files via upload

--- revisedfinalproject.py
+++ revisedfinalproject.py
@@ -1,4 +1,3 @@
-#choice function
 def stylechoice():
     style = input ('What type of style do you want? Type the number in front of whichever style you want. (1) APA, (2) MLA, (3) Chicago ')
     if style == 1:
@@ -7,6 +6,62 @@ def stylechoice():
         print ('MLA')
     elif style == 3:
         print ('Chicago')
-    return style
+    
+if style == '2':
+    first = input ('What type of source are you citing? book, website, a journal article, or an online video/film? ')
+   
+    if first == 'book':
+        title = input ('Enter the book title ')
+        name = input('Enter the authors first name ')
+        name2 = input ('Enter the authors last name ')
+        publisher = input ('Enter the books publisher ')
+        date = input ('Enter the year of publishing ')
+        print ('Here is your final citation ')
+        print (str(name2 + ', ') + str(name + '. ') + str(title + '. ') + str(publisher+ ', ') + str(date + '.' ))
 
-print ('Your citation style is ' + str(stylechoice()))
+
+    elif first == 'website':
+        title = input ('Enter the title of the website ')
+        name = input ('Enter the name of the article ')
+        authornf = input ('Enter the authors first name ')
+        authornl = input ('Enter the authors last name ')
+        date = input ('Enter the date the article/website was published ')
+        url = input ('Enter the articles url ')
+       
+        print ('Here is your final citation ')
+        print (str(authornl) + ', ' + str(authornf) + '. ' + ('"') + str(name)+ ('" ') + str(title + ', ') 
+        + str(date + ', ') + str(url))
+
+
+elif style == '3':
+    first = input ('What type of source are you citing? A book, website, or journal? ')
+
+
+    if first == 'book':
+        name1 = input('Enter the authors first name ')
+        name2 = input ('Enter the authors last name ')
+        title = input ('Enter the title of the book ')
+        state = input ('Enter the state of publishing ')
+        publisher = input ('Enter the publisher ')
+        date =  input ('Enter the date of publishment ')
+
+
+        print ('Here is your final citation ')
+        print (str(name2 + ', ') + str(name1 + ', ') + str(title + '. ') + str(state + ': ') + str(publisher + ', ') + str(date + '.'))
+
+
+elif style == '1':
+    first = input ('What type of source are you citing? A website, ')
+
+    if first == 'website':
+        name1 = input ('Enter the authors first name ')
+        name2 = input ('Enter the authors last name ')
+        year = input ('Enter the year the article was written ')
+        month = input ('Enter the year the article was written in english, not numbers ')
+        day = input ('Enter what day the article was written, in number(s) ')
+        motto = input ('Enter the website/articles subtitle/motto with punctuation ')
+        name = input ('Enter the websites name ')
+        url = input ('Enter the url/link to the website ')
+        print ('Here is your final citation! ')
+        print (str(name2 + ', ') + str(name1[0] + '. ') + '(' + str(year + ', ') + str(month + ' ') + str(day + '). ')
+         + str(motto + '. ') + str(name + '. ') + str(url) + '.')
